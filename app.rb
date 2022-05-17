6c0b50da48b8a7118bf708e26a2ed0ebd23d9db0
kasper-edblad-nti-johanneberg <74712684+kasper-edblad-nti-johanneberg@users.noreply.github.com>
2022-05-17 23:20:29 +0200

    Färdigt.
    Färdigt projekt Wsp 2022.

--- app.rb
+++ app.rb
@@ -5,104 +5,222 @@ require 'bcrypt'
 require_relative './model.rb'
 enable :sessions
 
+include Model
+
+# Attempts to check if the client has authorization
+before do
+    if session[:auth] == nil && (request.path_info != '/login' && request.path_info != '/user' && request.path_info != '/showuserregister' && request.path_info != '/error' && request.path_info != '/' && request.path_info != '/showlogin' && request.path_info != '/admin' && request.path_info != '/showadminregister' && request.path_info != '/showaccount' && request.path_info != '/logout')
+      redirect('/error')
+    end
+end
+
+# Display Landing Page
+# Displays all tv-shows
 get("/") do
     @serier = index() 
     slim(:"serier/index")
 end
 
+# Displays an error message
+get("/error") do
+    slim(:error)
+end
+
+# Displays a register form
 get('/showuserregister') do
     slim(:user_register)
 end
 
+# Displays a register form
 get('/showadminregister') do
     slim(:admin_register)
 end
 
+# Displays a login form
 get('/showlogin') do
     slim(:login)
 end
-  
+
+#Displays user account
+get('/showaccount') do
+    id = session[:id]
+
+    if id_nil(id)
+        redirect('/error')
+    end
+    
+    @your_account = account(id)
+    @your_ratings = account_ratings(id)
+    @titles = index()
+    slim(:"serier/account")
+end
+
+# Login
 post('/login') do
-    username = params[:username]
-    password = params[:password]
-    login(username, password)
-    redirect('/')
-end
-  
+    if logTime(session[:stress], session[:timeLogged])
+        username = params[:username]
+        password = params[:password]
+        sessions = login(username, password)
 
-post('/usernew') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
-  
-    new_user_or_admin(username, password, password_confirm, 1)
-end
-
-post('/adminnew') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
-  
-    new_user_or_admin(username, password, password_confirm, 2)
+        if sessions == false
+            redirect('/error')
+        else
+            session[:id] = sessions[0]
+            session[:auth] = sessions[1]
+        end
+        redirect('/')
+    else
+        redirect('/showlogin')
+    end
 end
 
+
+# Creates new user
+# @param [String] username, the user username
+# @param [String] password, the user password
+# @param [String] password_confiem, the users confirmed password
+post('/user') do
+    if logTime(session[:stress], session[:timeLogged])
+        username = params[:username]
+        password = params[:password]
+        password_confirm = params[:password_confirm]
+
+        if isEmpty(username) || isEmpty(password) 
+            redirect('/error')
+        end
+        if new_user_or_admin(username, password, password_confirm, 1) == false
+            redirect('/error')
+        end
+        redirect('/')
+    else
+        redirect('/showuserregister')
+    end
+end
+
+#Creates new admin user
+# @param [String] username, the user username
+# @param [String] password, the user password
+# @param [String] password_confiem, the users confirmed password
+post('/admin') do
+    if logTime()
+        username = params[:username]
+        password = params[:password]
+        password_confirm = params[:password_confirm]
+
+        if isEmpty(username) || isEmpty(password) 
+            redirect('/error')
+        end
+        if new_user_or_admin(username, password, password_confirm, 2) == false
+            redirect('/error')
+        end
+        redirect('/')
+    else
+        redirect('/showadminregister')
+    end
+end
+
+#Logs out from current user
 get("/logout") do
     session[:id] = nil
     session[:auth] = nil
     redirect("/")
 end
 
+# Displays a form to post new tv-show
 get("/new") do 
-    @genres = new_show()
+    @genres = genres()
+    id = session[:id]
 
+    if id_nil(id)
+        redirect('/error')
+    end
+    
     slim(:"serier/new")
 end
 
+# Displays a edit tv-show form
 get("/serier/:id/update") do
     @editid = params[:id]
 
+    if ownership(session[:id], @editid, session[:auth], 2) == false
+        redirect('/error')
+    end
+
     @genres = genres()
     @edittitle = update()
-    
+
     slim(:"serier/edit")
 end
 
+# Deletes an existing tv-show
 post("/serier/:id/delete") do
     id = params[:id]
+
+    if ownership(session[:id], id, session[:auth], 2) == false
+        redirect('/error')
+    end
 
     delete(id)
     
     redirect('/')  
 end
 
+# Deletes an existing rating
+post("/serier/:title_id/rate_delete") do
+    title_id = params[:title_id]
+    user_id = session[:id]
+
+    rate_delete(title_id, user_id)
+    
+    redirect("/showaccount")  
+end
+
+# Displays a rate tv-show form
 get("/serier/:id/rate") do
     @rateid = params[:id]
+
+    if session[:id] == nil 
+        redirect('/error')
+    end
+
     @ratetitle = rate(@rateid)
 
     slim(:"serier/rate")
 end
 
-post("/newtvshow") do
+# Creates a new movie
+post("/tvshow") do
     tvshow = params[:tvshow]
     genre_id = params[:genre_id]
     link = params[:link]
+    user_id = session[:id]
 
-    new_show_post(tvshow, genre_id, link)
+    if isEmpty(tvshow)
+        redirect('/error')
+    end
+
+    new_show_post(tvshow, genre_id, link, user_id)
 
     redirect('/')
 end
 
+# Updates an existing tv-show
 post('/serie/:editid/update') do
     tvshow = params[:newtvshowname]
     genre_id = params[:genre_id]
     link = params[:newlink]
     editid = params[:editid]
 
+    if isEmpty(tvshow)
+        redirect('/error')
+    end
+
     update_post(tvshow, genre_id, link, editid)
     
     redirect('/')  
 end
 
+# Rating a existing tv-show
 post('/serie/:rateid/rate') do
     user_rate = params[:user_rate]
     title_id = params[:rateid]
--- model.rb
+++ model.rb
@@ -1,93 +1,187 @@
-def connect_to_db(path)
-    db = SQLite3::Database.new(path)
-    db.results_as_hash = true
-    return db
-end
-
-def index()
-    db = connect_to_db("db/database.db")
-
-    return db.execute("SELECT * FROM title")
-end
-
-def login(username, password)
-    db = connect_to_db("db/database.db")
-    result = db.execute("SELECT * FROM user WHERE username = ?",username).first
-    if result == nil
-        @login_error = true
-        slim(:login) 
+module Model    
+    # Attempts to open a new database connection
+    # @return [Array] containing all the data from the database
+    def connect_to_db(path)
+        db = SQLite3::Database.new(path)
+        db.results_as_hash = true
+        return db
     end
-    pwdigest = result["pwdigest"]
-    id = result["id"]
-    auth = result["authority"]
-    if BCrypt::Password.new(pwdigest) == password
-      session[:id] = id
-      session[:auth] = auth
-      redirect('/')
-    else
-        @login_error = true
-        slim(:login)
-    end
-end
 
-def new_user_or_admin(username, password, password_confirm, auth)
-    if(password == password_confirm)
-        password_digest = BCrypt::Password.create(password)
-        authority = auth
+    #Attempts to select all tv-show
+    # @update [Integer] containing the average rate of all rates on each tv-show
+    # @return [Array] containing all the restaurants from the database
+    # @see Model#connect_to_db
+
+    def index()
         db = connect_to_db("db/database.db")
-        db.execute("INSERT INTO user (username,pwdigest,authority) VALUES (?,?,?)",username,password_digest,authority)
-        redirect('/')
-    elsif auth == 1
-        @register_error = true
-        slim(:user_register)
-    elsif auth == 2
-        @register_error = true
-        slim(:admin_register)
+        titles = db.execute("SELECT * FROM title")
+        titles.each do |title|
+            avg_rate = db.execute("SELECT AVG(rating) FROM user_title_relation WHERE title_id = ?", title['id']).first['AVG(rating)']
+            db.execute("UPDATE title SET avg_rate = ? WHERE id = ?",avg_rate, title['id'])
+        end
+        titles = db.execute("SELECT * FROM title")
+        return titles
+    end
+
+    def average_rating()
+        db = connect_to_db("db/database.db")
+        db.execute("SELECT AVG(rating) FROM user_title_relation WHERE title_id = ?", title['id'])['AVG(rating)'].first
+    end
+
+    # Attempts to check if user can login
+    # @see Model#connect_to_db
+    def login(username, password)
+        db = connect_to_db("db/database.db")
+        result = db.execute("SELECT * FROM user WHERE username = ?",username).first
+        if result == nil
+            return false 
+        end
+        pwdigest = result["pwdigest"]
+        id = result["id"]
+        auth = result["authority"]
+        if BCrypt::Password.new(pwdigest) == password
+          return [id, auth]
+        else
+            return false
+        end
+    end
+
+    # Attempts to check if too many inputs are recieved in close proximity
+    # @param [Integer] latestTime, the latest logged time
+    # @return [Boolean] whether the inputs are recieved in close proximity
+    # @see Model#connect_to_db
+    def logTime(stress, timeLogged)
+        tempTime = Time.now.to_i
+
+        if timeLogged == nil
+            timeLogged = 0
+        end
+        difTime = tempTime - timeLogged
+
+        if difTime < 1.5
+            timeLogged = tempTime
+            stress = true
+            return false
+        else
+            timeLogged = tempTime
+            stress = false
+            return true
+        end
+    end
+
+    # Attempts to register user
+    # @param [String] password, the password input
+    # @param [String] username, the user username
+    # @return [Boolean] whether the user registration succeeds 
+    # @see Model#connect_to_db
+    def new_user_or_admin(username, password, password_confirm, auth)
+        if(password == password_confirm)
+            password_digest = BCrypt::Password.create(password)
+            authority = auth
+            db = connect_to_db("db/database.db")
+            db.execute("INSERT INTO user (username,pwdigest,authority) VALUES (?,?,?)",username,password_digest,authority)
+            return true
+        else
+            return false
+        end
+    end
+
+    def authority(session_id, session_auth, required_session_id, required_session_auth)
+        return session_id == required_session_id && session_auth == required_session_auth
+    end
+
+    # Attempts to check if the user is logged in or not
+    # @return [Boolean] true if the user is not logged in and false if the user is logged in
+    def id_nil(id)
+        return id == nil
+    end
+
+    #Attempts to select all user info
+    #@return [Hash] containing all user account information 
+    def account(id)
+        db = connect_to_db("db/database.db")
+        return db.execute("SELECT * FROM user WHERE id = ?", id).first
+    end
+
+    #Attempts to select all the users ratings
+    #@return [Hash] containing all the users ratings 
+    def account_ratings(id)
+        db = connect_to_db("db/database.db")
+        return db.execute("SELECT * FROM user_title_relation WHERE user_id = ?", id)
+    end
+    
+    #Attempts to select all the available genres
+    #@return [Hash] containing all the available genres
+    def genres()
+        db = connect_to_db("db/database.db")
+        return db.execute("SELECT * FROM genre")
+    end
+
+    # Attempts to create a new show in database
+    def new_show_post(tvshow, genre_id, link, user_id)
+        db = connect_to_db("db/database.db")
+        db.execute("INSERT INTO title (name, genre_id, link, user_id) VALUES (?,?,?,?)",tvshow,genre_id,link,user_id)
+    end
+
+    # Attempts to retrieve all show info with a specified id
+    #@return [Hash] containing all show info with a specified id
+    def update()
+        db = connect_to_db("db/database.db")
+        return db.execute("SELECT * FROM title WHERE id=?", @editid).first
+    end
+
+    # Attempts to edit an existing show in database
+    def update_post(tvshow, genre_id, link, editid)
+        db = connect_to_db("db/database.db")
+        db.execute("UPDATE title SET name = ?, genre_id = ?, link = ? WHERE id= ?",tvshow, genre_id, link, editid)
+    end
+
+    # Attempts to delete an existing show in database
+    def delete(id)
+        db = connect_to_db("db/database.db")
+        db.execute("DELETE FROM title WHERE id='#{id}'")
+        db.execute("DELETE FROM user_title_relation WHERE title_id='#{id}'")
+    end
+
+    def rate(rateid)
+        db = connect_to_db("db/database.db")
+        db.execute("SELECT * FROM title WHERE id=?", rateid).first
+    end
+
+    # Attempts to check if the user id is a owner of the show or if the user has the correct authorization
+    # @return [Boolean] whetever the above stated is true
+    def ownership(user_id, show_id, authority, exception_authority)
+        db = connect_to_db("db/database.db")
+        owner = db.execute("SELECT user_id FROM title WHERE id=?",show_id).first["user_id"]
+        if user_id == owner || authority == exception_authority
+            return true
+        else
+            return false
+        end
+    end
+
+    def rate_post(user_rate, title_id, user_id)
+        db = connect_to_db("db/database.db")
+
+        if db.execute("SELECT rating FROM user_title_relation WHERE user_id = ? AND title_id = ?", user_id, title_id).first == nil
+            db.execute("INSERT INTO user_title_relation (rating, user_id, title_id) VALUES (?,?,?)", user_rate, user_id, title_id)
+        else
+            db.execute("UPDATE user_title_relation SET rating = ? WHERE user_id = ? AND title_id = ?", user_rate, user_id, title_id)
+        end
+    end
+
+    def rate_delete(title_id, user_id)
+        db = connect_to_db("db/database.db")
+        db.execute("DELETE FROM user_title_relation WHERE title_id = ? AND user_id = ?", title_id, user_id) 
     end
 end
 
-def genres()
-    db = connect_to_db("db/database.db")
-    return db.execute("SELECT * FROM genre")
-end
-
-def new_show()
-    db = connect_to_db("db/database.db")
-
-    return db.execute("SELECT * FROM genre")
-end
-
-def new_show_post(tvshow, genre_id, link)
-    db = connect_to_db("db/database.db")
-    db.execute("INSERT INTO title (name, genre_id, link) VALUES (?,?,?)",tvshow,genre_id,link)
-end
-
-def update()
-    db = connect_to_db("db/database.db")
-    return db.execute("SELECT * FROM title WHERE id=?", @editid).first
-end
-
-def update_post(tvshow, genre_id, link, editid)
-    db = connect_to_db("db/database.db")
-    db.execute("UPDATE title SET name = ?, genre_id = ?, link = ? WHERE id= ?",tvshow, genre_id, link, editid)
-end
-
-def delete(id)
-    db = connect_to_db("db/database.db")
-    db.execute("DELETE FROM title WHERE id='#{id}'")
-end
-
-def rate(rateid)
-    db = connect_to_db("db/database.db")
-    db.execute("SELECT * FROM title WHERE id=?", rateid).first
-end
-
-def rate_post(user_rate, title_id, user_id)
-    db = connect_to_db("db/database.db")
-
-    if db.execute("SELECT rating FROM user_title_relation WHERE user_id = ? AND title_id = ?", user_id, title_id)[0] == nil
-        db.execute("INSERT INTO user_title_relation (rating, user_id, title_id) VALUES (?,?,?)", user_rate, user_id, title_id)
+def isEmpty(text)
+    if text == nil
+        return true
+    elsif text == "" || text.scan(/ /).empty? == false 
+        return true
     else
-        db.execute("UPDATE user_title_relation SET rating = ? WHERE user_id = ? AND title_id = ?", user_rate, user_id, title_id)
+        return false
     end
 end
